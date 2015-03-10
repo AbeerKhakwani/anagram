44757c750406cbbd1222ea2c3ea1c7b43390883d
Cynthia Wilhelm & abeer khakwani <student@epicodus.com>
2015-03-10 16:11:05 -0700

    Add partial match search capability

--- anagram_ex/src/Anagram.php
+++ anagram_ex/src/Anagram.php
@@ -1,37 +1,38 @@
 <?php
 
-    class Anagram
+class Anagram
+{
+
+
+    function anagramCheck($main_word, $userlist_to_compare)
     {
+        //create empty array; will be returned
+        $results = array();
 
+        //split main word into array of letters and sort
+        $main_split_word = str_split(strtolower($main_word));
 
-        function anagramCheck($main_word, $userlist_to_compare)
-        {
-            //create empty array; will be returned
-            $results = array();
+        //explode field data enter each word seperated by space into an array called $temp_user_list_array ;
+        $temp_user_list_array = explode(" ", $userlist_to_compare);
 
-            //split main word into array of letters and sort
-            $main_split_word = str_split(strtolower($main_word));
-            sort($main_split_word);
+        //compare each word in userlist to main word
+        foreach ($temp_user_list_array as $compare) {
+            //split each word in the exploded array and sort
+            $temp_word = str_split(strtolower($compare));
 
-            //explode field data enter each word seperated by space into an array called $temp_user_list_array ;
-            $temp_user_list_array = explode(" ", $userlist_to_compare);
+            //get intersection of letters in main and temp words
+            $intersection = array_intersect($main_split_word, $temp_word);
 
-            //compare each word in userlist to main word
-            foreach($temp_user_list_array as $compare) {
-
-
-                //split each word in the exploded array and sort
-                $temp_word = str_split(strtolower($compare));
-                sort($temp_word);
-
-                if($main_split_word == $temp_word) {
-                    array_push($results, true);
-                } else {
-                    array_push($results, false);
-                }
+            if (count($intersection) == count($main_split_word)) {
+                array_push($results, 'anagram');
+            } else if (count($intersection) > 0) {
+                array_push($results, 'partial');
+            } else {
+                array_push($results, 'no');
             }
-            return $results;
         }
-    }
+        return $results;
+   }
+}
 
 ?>
--- anagram_ex/tests/AnagramTest.php
+++ anagram_ex/tests/AnagramTest.php
@@ -6,87 +6,101 @@
     class AnagramTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_Match_letters_true(){
-            //Arrange
-            $test_Anagram= new Anagram;
-            $input1 = "a";
-            $input2 = "a";
+        // function test_Match_letters_true(){
+        //     //Arrange
+        //     $test_Anagram= new Anagram;
+        //     $input1 = "a";
+        //     $input2 = "a";
+        //
+        //     //Act
+        //     $result = $test_Anagram->anagramCheck($input1,$input2);
+        //
+        //     //Assert
+        //     $this->assertEquals(array(true), $result);
+        // }
+        //
+        // function test_matchLetters_false()
+        // {
+        //     //Arrange
+        //     $test_Anagram = new Anagram;
+        //     $input1 = "a";
+        //     $input2 = "b";
+        //
+        //     //Act
+        //     $result = $test_Anagram->anagramCheck($input1, $input2);
+        //
+        //     //Assert
+        //     $this->assertEquals(array(false), $result);
+        // }
+        //
+        // function test_matchLetters_two_words()
+        // {
+        //     //Arrange
+        //     $test_Anagram = new Anagram;
+        //     $input1 = "a";
+        //     $input2 = "a b";
+        //
+        //     //Act
+        //     $result = $test_Anagram->anagramCheck($input1, $input2);
+        //
+        //     //Assert
+        //     $this->assertEquals(array(true, false), $result);
+        // }
+        //
+        // function test_matchWords()
+        // {
+        //     $test_Anagram = new Anagram;
+        //     $input1 = "bread";
+        //     $input2 = "beard";
+        //
+        //     //Act
+        //     $result = $test_Anagram->anagramCheck($input1, $input2);
+        //
+        //     //Assert
+        //     $this->assertEquals(array(true), $result);
+        //
+        // }
+        //
+        // function test_matchWordsFAIL()
+        // {
+        //     $test_Anagram = new Anagram;
+        //     $input1 = "bread";
+        //     $input2 = "beord bread";
+        //
+        //     //Act
+        //     $result = $test_Anagram->anagramCheck($input1, $input2);
+        //
+        //     //Assert
+        //     $this->assertEquals(array(false, true), $result);
+        //
+        // }
+        //
+        // function test_checkIn_One_Feild()
+        // {
+        //     //Arrange
+        //     $test_Anagram = new Anagram;
+        //     $input1 = "bread";
+        //     $input2 = "read beard";
+        //
+        //     //Act
+        //     $result = $test_Anagram->anagramCheck($input1, $input2);
+        //
+        //     //Assert
+        //     $this->assertEquals(array(false,true), $result);
+        // }
 
-            //Act
-            $result = $test_Anagram->anagramCheck($input1,$input2);
-
-            //Assert
-            $this->assertEquals(array(true), $result);
-        }
-
-        function test_matchLetters_false()
+        function test_checkPartialMatches()
         {
             //Arrange
-            $test_Anagram = new Anagram;
-            $input1 = "a";
-            $input2 = "b";
-
-            //Act
-            $result = $test_Anagram->anagramCheck($input1, $input2);
-
-            //Assert
-            $this->assertEquals(array(false), $result);
-        }
-
-        function test_matchLetters_two_words()
-        {
-            //Arrange
-            $test_Anagram = new Anagram;
-            $input1 = "a";
-            $input2 = "a b";
-
-            //Act
-            $result = $test_Anagram->anagramCheck($input1, $input2);
-
-            //Assert
-            $this->assertEquals(array(true, false), $result);
-        }
-
-        function test_matchWords()
-        {
             $test_Anagram = new Anagram;
             $input1 = "bread";
-            $input2 = "beard";
+            $input2 = "beard read jog ear";
 
             //Act
             $result = $test_Anagram->anagramCheck($input1, $input2);
 
             //Assert
-            $this->assertEquals(array(true), $result);
-
-        }
-
-        function test_matchWordsFAIL()
-        {
-            $test_Anagram = new Anagram;
-            $input1 = "bread";
-            $input2 = "beord bread";
-
-            //Act
-            $result = $test_Anagram->anagramCheck($input1, $input2);
-
-            //Assert
-            $this->assertEquals(array(false, true), $result);
-
-        }
-
-        function test_checkIn_One_Feild()
-        {
-            //Arrange
-            $test_Anagram = new Anagram;
-            $input1 = "bread";
-            $input2 = "read beard";
-
-            //Act
-            $result = $test_Anagram->anagramCheck($input1, $input2);
-
-            //Assert
-            $this->assertEquals(array(false,true), $result);
+            $this->assertEquals(array('anagram', 'partial', 'no', 'partial'), $result);
         }
     }
 
